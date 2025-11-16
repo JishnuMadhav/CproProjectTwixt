f8cb12229efc718dcfb317189b4d002fe05bdbb0
JishnuMadhav <85347354+JishnuMadhav@users.noreply.github.com>
2025-11-16 23:41:13 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 int main(){
     int board[24][24];
     setboard(board);
-
     int turn=1,row,col;
 
     while(1){
         printboard(board);
+
         if(turn==1)printf("Red's turn enter row and column:");
-        else printf("Black's turn enter row and column:");
+        else printf("Blue's turn enter row and column:");
 
         scanf("%d %d",&row,&col);
         row=row-1;
@@ -20,6 +20,14 @@ int main(){
             continue;
         }
 
+        if((row==0||row==23)&&turn!=1){
+            printf("Only Red can place on the top or bottom rows\n");
+            continue;
+        }
+        if((col==0||col==23)&&turn!=2){
+            printf("Only Blue can place on the left or right columns\n");
+            continue;
+        }
         int res=place(board,row,col,turn);
         if(res==-1){
             printf("Cannot place pin in the corners\n");
@@ -29,7 +37,6 @@ int main(){
             printf("Cell Already Occupied\n");
             continue;
         }
-
         if(turn==1)turn=2;
         else turn=1;
     }
