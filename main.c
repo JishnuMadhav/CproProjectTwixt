e2795c5fda79de6e30d92d3690099462213990ba
JishnuMadhav <85347354+JishnuMadhav@users.noreply.github.com>
2025-11-16 21:44:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,36 @@
 #include "board.h"
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-    Board b;
-    setboard(&b);
-     
-    int turn = 1;
-    int row, col;
-    while (1){
-        printboard(&b);
-        printf("%s's turn enter row and column:",(turn == 1 ? "Red":"Black"));
-        scanf("%d %d", &row, &col);
-        row = row-1;
-        col = col-1;
+    int board[24][24];
+    setboard(board);
 
-        if (row < 0 || row >= 24 || col < 0 || col >= 24) {
+    int turn=1,row,col;
+
+    while(1){
+        printboard(board);
+        if(turn==1)printf("Red's turn enter row and column:");
+        else printf("Black's turn enter row and column:");
+
+        scanf("%d %d",&row,&col);
+        row=row-1;
+        col=col-1;
+        if(row<0||row>=24||col<0||col>=24){
             printf("Please enter values between 1 and 24.\n");
             continue;
         }
 
-        if(place(&b,row,col,turn)){
-            if(turn == 1){
-                turn =2;
-            }
-            else{
-                turn = 1;
-            }
+        int res=place(board,row,col,turn);
+        if(res==-1){
+            printf("Cannot place pin in the corners\n");
+            continue;
         }
-        else{
+        if(res==0){
             printf("Cell Already Occupied\n");
+            continue;
         }
-    } 
+
+        if(turn==1)turn=2;
+        else turn=1;
+    }
 }
